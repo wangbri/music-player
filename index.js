3e03a9ba6829172b3bebfa2953e655c09cdf49cb
Brian Wang <wangbri@gmail.com>
2018-05-16 23:51:02 -0500

    add spotify webplayer and heroku
    setup conflicts with bootstrap

---
+++ index.js
@@ -0,0 +1,22 @@
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+var app = express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index'))
+  .get('/cool', (req, res) => res.send(cool()))
+
+app.get('/times', (req, res) => {
+  let result = ''
+  const times = process.env.TIMES || 5
+  for (i = 0; i < times; i++) {
+    result += i + ' '
+  }
+  res.send(result)
+})
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
