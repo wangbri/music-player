49bdb44fd407533f50b65100cc3dc58d607f5da5
Brian Wang <wangbri@gmail.com>
2018-06-05 00:08:35 -0500

    move client/app auth to server side for security, add socketio

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const cool = require('cool-ascii-faces');
 const express = require('express');
 const path = require('path');
+const cool = require('cool-ascii-faces');
+const request = require('request');
+const querystring = require('querystring');
+
 const PORT = process.env.PORT || 5000;
+const client_id = "25fcb6d7f797411a92f805b55f27555d";
+const client_secret = "a200f34bb0434a91b8197891f47cb135";
+const redirect_uri = "http://localhost:5000/cool";
 
 var app = express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -10,13 +16,65 @@ var app = express()
   .get('/', (req, res) => res.render('index'))
   .get('/cool', (req, res) => res.send(cool()));
 
-// app.get('/times', (req, res) => {
-//   let result = ''
-//   const times = process.env.TIMES || 5
-//   for (i = 0; i < times; i++) {
-//     result += i + ' '
-//   }
-//   res.send(result)
-// })
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+server.listen(PORT);
+
+io.on('connection', function(socket) {
+	socket.on('login', function(data) {
+
+	  	// application requests authorization
+		var scope = 'user-read-private user-read-email';
+		var authUrl = 'https://accounts.spotify.com/authorize?' +
+			querystring.stringify({
+				response_type: 'code',
+				client_id: client_id,
+				scope: scope,
+				redirect_uri: redirect_uri,
+				show_dialog: true
+			});
+
+		io.emit('login', authUrl);
+	});
+
+	socket.on('access', function(req, res) {
+
+		// application requests refresh and access tokens
+		// after checking the state parameter
+		var authOptions = {
+			url: 'https://accounts.spotify.com/api/token',
+			form: {
+				code: code,
+				redirect_uri: redirect_uri,
+				grant_type: 'authorization_code'
+			},
+			headers: {
+				'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+			},
+			json: true
+		};
+
+		request.post(authOptions, function(error, response, body) {
+			if (!error && response.statusCode === 200) {
+				var access_token = body.access_token;
+				var refresh_token = body.refresh_token;
+
+				var options = {
+					url: 'https://api.spotify.com/v1/me',
+					headers: {'Authorization': 'Bearer ' + access_token},
+					json: true
+				};
+
+				// use the access token to access the Spotify Web API
+				request.get(options, function(error, response, body) {
+					console.log(body);
+				});
+
+				io.emit('access', access_token)
+			}
+		});
+	});
+});
+
+//app.listen(PORT, () => console.log(`Listening on ${PORT}`));
